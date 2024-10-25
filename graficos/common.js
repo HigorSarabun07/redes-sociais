47b842bde162a7c1b8ced5c47cb7b51d72a7c95d
@motomotorzão <103429503+HigorSarabun07@users.noreply.github.com>
2024-10-25 08:50:36 -0300

    .

--- graficos/common.js
+++ graficos/common.js
@@ -1,7 +1,12 @@
 const getCSS = (variavel) => {
     const bodyStyles = getComputedStyle(document.body)
     return bodyStyles.getPropertyValue(variavel)
-    return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
-export { getCSS }
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
